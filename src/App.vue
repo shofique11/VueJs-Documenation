624c48abf8480664a47b25f6e07a538223880161
shofique11 <programmingcode11@gmail.com>
2024-11-29 18:29:51 +0600

    install vue js

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Test from './components/Test.vue'
+</script>
+
+<template>
+  <main>
+    <Test mytestmsg="How are you ?" />
+    <CountCompontent />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    mytestmsg: {
+        type: String,
+        required: true,
+    },
+})
+</script>
+<template>
+    <div class="greetings">
+        <h1 class="green">{{ mytestmsg }}</h1>
+    </div>
+</template>
